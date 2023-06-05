1192d12da1916ca4c9d27d0ede215fadf2e5b609
rmbortone <rmbortone@yahoo.com.br>
2023-06-05 20:38:48 -0300

    Atualização 1

---
+++ src/main/kotlin/dev/bortone/request/credit/system/ennumeration/RequestCreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package dev.bortone.request.credit.system.ennumeration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestCreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestCreditSystemApplication>(*args)
+}
---
+++ src/test/kotlin/dev/bortone/request/credit/system/RequestCreditSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.bortone.request.credit.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestCreditSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
